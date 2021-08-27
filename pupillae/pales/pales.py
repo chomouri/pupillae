46a67f0a33bda5c401bb08435a2f598339514d63
chomouri <jason.matthew.henson@gmail.com>
2021-08-27 23:57:42 +1200

    pales/*.py docstrings/typing. Version 1.

--- pupillae/pales/pales.py
+++ pupillae/pales/pales.py
@@ -1,5 +1,5 @@
 import os
-from typing import Any, List, Optional, Tuple
+from typing import Any, List, Optional, Tuple, Dict
 import re
 import sys
 
@@ -9,13 +9,19 @@ from psycopg2 import sql
 import parser
 from pupillae.conf import config
 
-def get_cols():
-    """ Stand-in function to get database tables/columns """
+def get_cols() -> Dict[str, Any]:
+    """Stand-in function to get database tables/columns.
 
+    For each table, compiles a dictionary of accepted column names and
+    assigns a nested dictionary of default values to each column.
+
+    """
     defaults = {"query_index": None, "show": False, "orientation": "ASC"}
     tables = ["manufactors", "modifactors"]
-    table_cols = [["material", "company", "m_name"],
-        ["based", "painted", "m_class", "m_arms", "m_armor", "m_race", "m_type", "squad_reps", "squad_name"]]
+    table_cols = [
+        ["material", "company", "m_name"],
+        ["based", "painted", "m_class", "m_arms", "m_armor", "m_race", "m_type", "squad_reps", "squad_name"]
+        ]
     table_dict = {}
 
 # Build column dictionaries with default values.
@@ -28,8 +34,38 @@ def get_cols():
             table_dict[col].update(defaults)
     return table_dict
 
-def query_db(cur, db_query: str, offset: Optional[dict]):
-    """ Determine the purpose of the query and calls the correct function """
+def query_db(cur, db_query: str, offset: Optional[Dict[str, int]]):
+    """Determine the purpose of query and calls the correct function.
+
+    Parameters
+    ----------
+    cur :
+        The cursor to be used for the query.
+    db_query : str
+        The message to use for the query. `db_query` uses the following
+        syntax and format (by way of indices):
+        "FFFF Q...".
+        Where:
+        "FFFF" is a **four** letter word that is used to call a particular
+        function.
+        "Q..." is the part that will be passed to the correct function.
+    offset : dict, optional
+        The offset is a dictionary used in SQL SELECT queries to offset
+        searches and limit returns and with the following keys:
+        "limit_current": int
+        "offset_current": int
+
+    Returns
+    -------
+    See:
+        pales.db_find
+        pales.db_show
+
+    Raises
+    ------
+    TODO: Error implementation.
+    ^ Uses return as error string.
+    """
     function, params = db_query[:4], db_query[5:]
     print(function, params)
     if function == "find":
@@ -41,8 +77,27 @@ def query_db(cur, db_query: str, offset: Optional[dict]):
     else: results = f"""Malformed query: Needs "$db find " or "$db show"."""
     return results
 
-def db_find(cur, db_query, offset_dict):
-    """ Build a SQL query by sending the parameters to the appropriate function """
+def db_find(cur, db_query: str, offset_dict: Dict[str, int]) -> str:
+    """Build a SQL query and execute that query.
+
+    `db_query` is passed to parser.build_q_dict to be tokenised, then
+    passed to parser.parse_f_dict to build a complete query. The query
+    is executed and the results are returned.
+
+    Parameters
+    ----------
+    cur : psycopg cursor
+    db_query : str
+    offset_dict : dict
+        Uses the following keys:
+            "limit_current": int
+            "offset_current": int
+
+    Raises
+    ------
+    TODO: Error implementation.
+    ^ Uses return as error string.
+    """
     error = None
     parsed = None
     msg = f"Searching for: {db_query}"
@@ -76,8 +131,23 @@ def db_find(cur, db_query, offset_dict):
                 return error
             return msg
 
-def db_show(cur, model_id):
-    """ Check for valid Model_ID. Return the full path of image requested """
+def db_show(cur, model_id: str) -> str:
+    """Validates Model_ID and returns image path if present.
+
+    Parameters
+    ----------
+    cur : psycopg cursor
+    model_id : str
+        XXXX_YYYY format of hex(m_id + p_id)
+
+    Returns
+    -------
+    str
+        Either a string detailing the error,
+        or a string of `model_id` and the fully qualified path to the
+        .jpg, joined with "||".
+        Images are tested for existance, not access.
+    """
     model_id = model_id.upper()
     if re.fullmatch(r'[0-9A-F]{4}_[0-9A-F]{4}', model_id):
         img_params = config.config(section="fons_gui")
--- pupillae/pales/query_builder.py
+++ pupillae/pales/query_builder.py
@@ -1,21 +1,45 @@
-# This script uses the table_dict, select_list, order_dict, where_dict from the
-# parser to build queries.
+# This script uses the table_dict and order_dict from the parser to
+# build queries.
 #
-# Since some of the variables used in building the query are PostgreSQL keywords,
-# the scripts in which to build them are in this file.
+# Since some of the variables used in building the query are PostgreSQL
+# keywords, the scripts in which to build them are in this file.
 #
-# This script should not be considered to completely santitise the input, instead
-# relying on the parser to pass it santitised data, but between the parser and
-# query_builder, should be closely guarded as potential avenues for SQL Injections.
+# This script should not be considered to completely santitise the
+# input, instead relying on the parser to pass it santitised data, but
+# between the parser and query_builder, should be closely guarded as
+# potential avenues for SQL Injections.
 
+# Note:
 # table_dict, select_list, order_dict, where_dict
 # ("SELECT * FROM mytable WHERE path LIKE %s ESCAPE ''", (path,))
+
 import psycopg2
 from psycopg2 import sql
 
-def build_select(cur, table_dict):
-    """ Builds the SELECT statement using the table_dict's "show" values """
+def build_select(cur, table_dict: dict) -> str:
+    """Build the SELECT statement using the table_dict's "show" values
 
+    Using the hex value of the primary key (p_id) and the foreign key
+    (m_id) as a base, the SQL query is built with the `table_dict`
+    keys (column) for each nested key (show) that has the value: True
+
+    If no columns have their nested dictionary value for "show" as
+    True, then a default selection is included: Model_ID, m_name and
+    squad_name.
+
+    Parameters
+    ----------
+    table_dict : dict
+        Relevant keys:
+            column (variable) : str
+                Relevant Nested keys:
+                    show : bool
+
+    Returns
+    -------
+    str
+        SQL SELECT query psycopg.as_string format.
+    """
     select_list = []
     for column in table_dict.keys():
         if table_dict[column]["show"] == True:
@@ -36,12 +60,28 @@ INNER JOIN modifactors ON manufactors.m_id = modifactors.m_id
 
     return select_from
 
-def build_order(cur, order_dict):
-    """ Builds the ORDER BY and LIMIT/OFFSET statement using the
-    order_dict's orientation values.
+def build_order(cur, order_dict: dict) -> str:
+    """Build the ORDER BY and LIMIT/OFFSET SQL Parameters.
 
-    Notes: It appears that the columns from order_dict do not need to be quoted """
+    Uses the order_dict's orientation values.
 
+    Parameters
+    ----------
+    order_dict : dict
+        Keys:
+            column (variable) : str
+        Values:
+            orientation : str {"DESC", "ASC"}
+
+    Returns
+    -------
+    str
+        SQL ORDER BY/LIMIT/OFFSET query psycopg.as_string format.
+
+    Notes
+    -----
+    It appears that the columns from order_dict do not need to be quoted
+    """
     order_list = []
     if len(order_dict) < 1:
         order_by = 'ORDER BY "Model_ID" ASC'
